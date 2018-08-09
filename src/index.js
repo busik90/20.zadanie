a159c95e59119ac8c8bdce04ab8294a0d374db68
Pawel Bus <pawelbus.dev@outlook.com>
2018-08-09 15:04:21 +0200

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+
 import store from './store';
+import routes from './routes';
 import DevTools from './DevTools';
 
 // Simple test
@@ -10,10 +13,8 @@ store.dispatch(countries.getCountries());
 
 render(
   <Provider store={store}>
-    <div>
-      <h1>Inicjalizacja projektu</h1>
-      <DevTools />
-    </div>
+    <Router history={hashHistory} routes={routes}>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
